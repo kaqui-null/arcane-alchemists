0952c261290fbaf0148ea5338b1071b47c501391
pedromonteir1111 <pmfm2004@gmail.com>
2025-05-31 23:34:57 -0300

    sidescroller enemy and kill it by shooting

---
+++ scripts/line_enemy.gd
@@ -0,0 +1,36 @@
+extends CharacterBody3D
+
+@onready var left_raycast = $LeftEdgeCast
+@onready var right_raycast = $RightEdgeCast
+@export var speed = 0.5
+
+var health = 100
+var facing_direction = -1
+
+func _physics_process(delta):
+	if is_on_floor():
+		if !left_raycast.is_colliding() and facing_direction == -1:
+			flip_direction()
+		elif !right_raycast.is_colliding() and facing_direction == 1:
+			flip_direction()
+			
+	velocity.y += get_gravity().y * delta
+	velocity.x = speed * facing_direction
+	move_and_slide()
+
+func take_damage(damage):
+	health -= damage
+	if health <= 0:
+		die()
+
+func die():
+	queue_free()
+
+func flip_direction():
+	facing_direction *= -1
+
+func _on_left_area_body_entered(body: Node3D) -> void:
+	facing_direction = 1
+
+func _on_right_area_body_entered(body: Node3D) -> void:
+	facing_direction = -1
--- scripts/magic.gd
+++ scripts/magic.gd
@@ -2,6 +2,7 @@ extends Area3D
 
 @export var speed = 10.0
 var direction = Vector3.RIGHT  # ou LEFT, será substituído ao instanciar
+var damage = 5
 
 func _physics_process(delta):
 	position += direction * speed * delta
@@ -9,3 +10,9 @@ func _physics_process(delta):
 	# remove o projétil após sair da tela
 	if abs(global_position.x) > 1000:
 		queue_free()
+
+func _on_body_entered(body: Node3D) -> void:
+	if body.has_method("take_damage"):
+		body.take_damage(damage)
+	
+	queue_free()
